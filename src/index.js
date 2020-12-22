1e86d6c682952d5e549ff70c63a41e0be5f5d87a
thinh bui hong <thinhbuihong.ma@gmail.com>
2020-12-22 10:19:46 +0700

    51 login user function

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import ProductProvider from './context/products';
-import { CartProvider } from './context/cart';
+import ProductProvider from "./context/products";
+import { CartProvider } from "./context/cart";
+import { UserProvider } from "./context/user";
 
 ReactDOM.render(
-  <ProductProvider>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </ProductProvider>
-  , document.getElementById("root"));
+  <UserProvider>
+    <ProductProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ProductProvider>
+  </UserProvider>,
+
+  document.getElementById("root")
+);
