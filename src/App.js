9113be65aaafe6925ad805c2eef4d17d40ca12b8
thinhbuihong <thinhbuihong.ma@gmail.com>
2020-12-26 14:04:25 +0700

    >59

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-import Abount from './pages/About';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Error from './pages/Error';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Products from './pages/Products';
-import ProductDetails from './pages/ProductDetails';
-
-//component
-import Header from './components/Header';
-
+// react router dom
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// pages
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+// components
+import Header from "./components/Header";
+import Alert from "./components/Alert";
+import PrivateRoute from "./components/PrivateRoute";
+import ScrollButton from "./components/ScrollButton";
 export default function App() {
-  return (<Router>
-    <Header></Header>
-    <Switch>
-      <Route path="/" exact>
-        <Home></Home>
-      </Route>
+  return (
+    <Router>
+      <Header />
+      <Alert />
+      <ScrollButton />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <PrivateRoute path="/checkout" name="john" msg="hello">
+          <Checkout />
+        </PrivateRoute>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route
+          path="/products/:id"
+          children={<ProductDetails></ProductDetails>}
+        ></Route>
 
-      <Route path="/about">
-        <Abount></Abount>
-      </Route>
-
-      <Route path="/cart">
-        <Cart></Cart>
-      </Route>
-
-      <Route path="/checkout">
-        <Checkout></Checkout>
-      </Route>
-
-      <Route path="/login">
-        <Login></Login>
-      </Route>
-
-      <Route exact path="/products">
-        <Products></Products>
-      </Route>
-
-      <Route path="/products/:id" children={<ProductDetails></ProductDetails>}>
-      </Route>
-
-      <Route path="*">
-        <Error></Error>
-      </Route>
-
-    </Switch>
-  </Router>);
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
--- src/context/cart.js
+++ src/context/cart.js
@@ -1,81 +1,83 @@
-// cart context
-import React from 'react';
-import localCart from '../utils/localCart';
-
-
+import React from "react";
+// import localCart from "../utils/localCart";
+import reducer from "./reducer";
+import { REMOVE, INCREASE, DECREASE, ADD_TO_CART, CLEAR_CART } from "./actions";
 function getCartFromLocalStorage() {
-  return localStorage.getItem('cart') ?
-    JSON.parse(localStorage.getItem('cart')) : []
+  return localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : [];
 }
 
 const CartContext = React.createContext();
 
 function CartProvider({ children }) {
-  const [cart, setCart] = React.useState(getCartFromLocalStorage());
+  const [cart, dispatch] = React.useReducer(reducer, getCartFromLocalStorage());
   const [total, setTotal] = React.useState(0);
   const [cartItems, setCartItems] = React.useState(0);
 
   React.useEffect(() => {
-    //local storage
-    localStorage.setItem('cart', JSON.stringify(cart));
-    //cartitems
-    let newCartItems = cart.reduce((acc, curr) => {
-      return acc + curr.amount;
+    // local storage
+    localStorage.setItem("cart", JSON.stringify(cart));
+    // cart items
+    let newCartItems = cart.reduce((total, cartItem) => {
+      return (total += cartItem.amount);
     }, 0);
-    setCartItems(newCartItems)
-
-    //cart total
-    let newTotal = cart.reduce((acc, curr) => {
-      return acc + curr.amount * curr.price;
+    setCartItems(newCartItems);
+    // cart total
+    let newTotal = cart.reduce((total, cartItem) => {
+      return (total += cartItem.amount * cartItem.price);
     }, 0);
     newTotal = parseFloat(newTotal.toFixed(2));
     setTotal(newTotal);
-  }, [cart])
+  }, [cart]);
 
-  //////////////////
+  // remove item
   const removeItem = id => {
-    setCart([...cart].filter((item) => {
-      return item.id !== id;
-    }))
-  }
+    dispatch({ type: REMOVE, payload: id });
+  };
+  // increase amount
   const increaseAmount = id => {
-    const newCart = [...cart].map(item => {
-      return item.id === id ? { ...item, amount: item.amount + 1 } : { ...item };
-    })
-    setCart(newCart);
-  }
+    dispatch({ type: INCREASE, payload: id });
+  };
+  // decrease amount
   const decreaseAmount = (id, amount) => {
     if (amount === 1) {
-      removeItem(id);
+      dispatch({ type: REMOVE, payload: id });
       return;
+    } else {
+      dispatch({ type: DECREASE, payload: id });
     }
-    const newCart = [...cart].map(item => {
-      return item.id === id ? { ...item, amount: item.amount - 1 } : { ...item };
-    })
-    setCart(newCart);
-  }
+  };
+  // add to cart
   const addToCart = product => {
-    const { id, image: { url }, title, price } = product;
-    const item = [...cart].find(item => item.id === id);
+    let item = [...cart].find(item => item.id === product.id);
     if (item) {
-      increaseAmount(id);
-      return;
+      dispatch({ type: INCREASE, payload: product.id });
+    } else {
+      dispatch({ type: ADD_TO_CART, payload: product });
     }
-    //not in cart
-    const newItem = { id, image: url, title, price, amount: 1 };
-    const newCart = [...cart, newItem];
-    setCart(newCart);
-  }
-  const clearCart = id => {
-    setCart([]);
-  }
+  };
+  // clear cart
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
 
-  return <CartContext.Provider value={{
-    cart, total, cartItems, removeItem,
-    increaseAmount, decreaseAmount, addToCart, clearCart
-  }}>
-    {children}
-  </CartContext.Provider>
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        total,
+        cartItems,
+        removeItem,
+        increaseAmount,
+        decreaseAmount,
+        addToCart,
+        clearCart
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
 
 export { CartContext, CartProvider };
